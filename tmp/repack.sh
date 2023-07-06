1ac2870154a3b9761270ba235cd1ddcd64717744
iotah <writeforever@foxmail.com>
2023-07-06 17:41:35 +0800

    firmware makes f1c100s booting from spinand
    Signed-off-by: iotah <writeforever@foxmail.com>

--- tmp/repack.sh
+++ tmp/repack.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+UBOOT_OFFSET=0
+DTB_OFFSET=1024
+KERNEL_OFFSET=1152
+ROOTFS_OFFSET=5248
+
 rm ninjar-lite-spinand-img.bin
 dd if=u-boot-sunxi-with-spl-spinand.bin of=ninjar-lite-spinand-img.bin bs=1K conv=notrunc
-#dd if=uboot-with-spl-spinand.bin of=ninjar-lite-spinand-img.bin bs=1K conv=notrunc
-dd if=suniv-f1c100s-licheepi-nano.dtb of=ninjar-lite-spinand-img.bin bs=1K seek=1024 conv=notrunc
-dd if=zImage of=ninjar-lite-spinand-img.bin bs=1K seek=1088 conv=notrunc
+dd if=suniv-f1c100s-licheepi-nano.dtb of=ninjar-lite-spinand-img.bin bs=1K seek=${DTB_OFFSET} conv=notrunc
+dd if=zImage of=ninjar-lite-spinand-img.bin bs=1K seek=${KERNEL_OFFSET} conv=notrunc
+#dd if=rootfs.ubifs of=ninjar-lite-spinand-img.bin bs=1K seek=${ROOTFS_OFFSET} conv=notrunc
+dd if=rootfs.jffs2 of=ninjar-lite-spinand-img.bin bs=1K seek=${ROOTFS_OFFSET} conv=notrunc
