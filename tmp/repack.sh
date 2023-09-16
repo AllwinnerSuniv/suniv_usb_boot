6e0d8558c427c6bc3c19624cb831f30cb7a8e4a2
iotah <writeforever@foxmail.com>
2023-09-16 18:18:10 +0800

    update files
    Signed-off-by: iotah <writeforever@foxmail.com>

---
+++ tmp/repack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+UBOOT_OFFSET=0
+DTB_OFFSET=1024
+KERNEL_OFFSET=1152
+ROOTFS_OFFSET=6272
+
+rm ninjar-lite-spinand.img
+dd if=u-boot-sunxi-with-spl-spinand.bin of=ninjar-lite-spinand.img bs=1K conv=notrunc
+dd if=suniv-f1c100s-licheepi-nano.dtb of=ninjar-lite-spinand.img bs=1K seek=${DTB_OFFSET} conv=notrunc
+dd if=zImage of=ninjar-lite-spinand.img bs=1K seek=${KERNEL_OFFSET} conv=notrunc
+dd if=rootfs.ubi of=ninjar-lite-spinand.img bs=1K seek=${ROOTFS_OFFSET} conv=notrunc
+
